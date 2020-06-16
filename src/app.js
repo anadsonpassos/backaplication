d43699b70dfaaeb4278b41afe5db449790df4b06
Anadson Passos <anadson.passos@infracommerce.com.br>
2020-06-15 22:38:45 -0300

    Ajustes

--- src/app.js
+++ src/app.js
@@ -22,20 +22,20 @@ app.use(express.urlencoded( { extended: true }));
 app.use(routes);
 app.use(errors());
 
-app.use((req, res, next) => {
-    allowedOrigins = ['http: // localhost: 3000', 'http://localhost:3001', 'https://agiltech.herokuapp.com'];
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Header',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
+// app.use((req, res, next) => {
+//     allowedOrigins = ['http: // localhost: 3000', 'http://localhost:3001', 'https://agiltech.herokuapp.com'];
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header(
+//         'Access-Control-Allow-Header',
+//         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//     );
   
-      if (req.method === 'OPTIONS') {
-          res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-          return res.status(200).json({});
-    }
-    next();
-  });
+//       if (req.method === 'OPTIONS') {
+//           res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//           return res.status(200).json({});
+//     }
+//     next();
+//   });
 
 
 module.exports = app;
